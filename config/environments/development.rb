f07074cac7fe0693ad129a916619fc4aff5db0ef
ken <a@a.com>
2018-09-02 21:21:26 +0900

    production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true     #trueに設定すると動的コンパイルが「有効」
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
