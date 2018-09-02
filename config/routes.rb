9d349502cc6695df377fbaf1c96abdac740f4ec5
ken <a@a.com>
2018-09-02 23:08:01 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     get 'keijis/kanri', to:'keijis#kanri', as:'kanri_keiji'
     resources :categories
     resources :keijis
-    resources :users :only => [ :new, :show ]
+    resources :users, :only => [ :new, :show ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
